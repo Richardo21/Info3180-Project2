abec7eda8395ce3e4e1a1fcc62b1023203e60f06
Damain Ross <damain.ross@mymona.uwi.edu>
2019-03-27 15:38:52 +0000

    first push

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,13 @@
+from flask_wtf import FlaskForm
+#from wtforms import StringField,Form,validators,SubmitField
+from wtforms.validators import DataRequired,Email
+from wtforms import StringField,TextField
+from wtforms import StringField, IntegerField, FileField, SelectField
+from wtforms.validators import InputRequired
 
+
+class UploadForm(FlaskForm):
+
+    description  = TextField(u'des',validators=[DataRequired()])
+    photo = FileField(u'pho',validators=[DataRequired()])
+      
